79be7c891f44d40910cbacae011cc594a4e3ea85
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-06-13 00:35:19 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import Feedback from './components/Feedback/Feedback'
+import { Component } from 'react';
+import Feedback from './components/Feedback/Feedback';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Feedback />
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+  render() {
 
+    const state = this.state;
+    console.log(Object.keys(state));
+    // const { state } = this.state
+    // console.log(state);
+    // console.log(this.state);
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <Feedback
+            state={state}
+          />
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -3,8 +3,24 @@ import s from './Feedback.module.css';
 
 export default class Feedback extends Component {
     render() {
+        const state = this.props.state;
+        const id = Object.keys(state)
+        console.log(this.props);
         return (
-            <div>Test</div>
+            <div>
+                <div>Please leave feedback</div>
+                <div>
+                    <button id={id[0]}>Good</button>
+                    <button id={id[1]}>Neutral</button>
+                    <button id={id[2]}>Bad</button>
+                </div>
+                <div>Statistics</div>
+                <ul>
+                    <li id={id[0]}>Good: {state.good}</li>
+                    <li id={id[1]}>Neutral: {state.neutral}</li>
+                    <li id={id[2]}>Bad: {state.bad}</li>
+                </ul>
+            </div>
         )
     }
 }
