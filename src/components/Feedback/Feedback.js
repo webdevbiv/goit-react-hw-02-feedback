b93a61cf13273d2d910d2352b222e1587524b949
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-06-12 23:00:14 +0300

    master

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import s from './Feedback.module.css';
+
+export default class Feedback extends Component {
+    render() {
+        return (
+            <div>Test</div>
+        )
+    }
+}
